7c6dd8893b17f0801bea6d5d0d15e320e694bcdc
KaranMakadiya757 <karanmakadiya87@gmail.com>
2024-04-24 16:32:39 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,181 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import "./App.css"
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { FaClipboard } from "react-icons/fa";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [password, setPassword] = useState("")
+  const [passwordLength, setPasswordLength] = useState(26)
+  const [includeUpperCase, setIncludeUpperCase] = useState(false)
+  const [includeLowerCase, setIncludeLowerCase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+
+  const numbers = '0123456789'
+  const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const specialCharacters = "!'^+%&/()=?_#$½§{[]}|;:>÷`<.*-@é"
+
+  const COPY_SUCCESS = "Password successfully copied to clipboard"
+  const COPY_Fail = "Password successfully copied to clipboard"
+
+  const handleGeneratePassword = () => {
+    if (!includeUpperCase && !includeLowerCase && !includeNumbers && !includeSymbols) {
+      notify("To generate password you must select atleast one checkbox", true)
+    }
+    else {
+      let characterList = ""
+      if (includeNumbers) {
+        characterList = characterList + numbers
+      }
+      if (includeUpperCase) {
+        characterList = characterList + upperCaseLetters
+      }
+      if (includeLowerCase) {
+        characterList = characterList + lowerCaseLetters
+      }
+      if (includeSymbols) {
+        characterList = characterList + specialCharacters
+      }
+      setPassword(createPassword(characterList))
+      notify("Password is generated successfully", false)
+    }
+
+
+  }
+  const createPassword = (characterList) => {
+    let password = ""
+    const characterListLength = characterList.length
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength)
+      password = password + characterList.charAt(characterIndex)
+    }
+    return password
+  }
+  const copyToClipboard = (password) => {
+
+    navigator.clipboard.writeText(password)
+  }
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      toast.error(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    else {
+      toast(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+
+  }
+  const handleCopyPassword = (e) => {
+    if (password === "") {
+      notify(COPY_Fail, true)
+    }
+    else {
+      copyToClipboard(password)
+      notify(COPY_SUCCESS)
+    }
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Karan</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator__header">
+            Password Generator
+          </h2>
+          <div className="generator__password">
+            <h3 >{password}</h3>
+            <button className="copy__btn">
+              <i onClick={handleCopyPassword} className="far fa-clipboard"><FaClipboard /></i>
+            </button>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-strength">Password length</label>
+            <input
+              className="pw"
+              defaultValue={passwordLength}
+              type="number"
+              id="password-stregth"
+              name="password-strength"
+              max="26"
+              min="8"
+              onChange={(e) => setPasswordLength(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Add Uppercase Letters</label>
+            <input
+              checked={includeUpperCase}
+              type="checkbox"
+              id="uppercase-letters"
+              name="uppercase-letters"
+              onChange={(e) => setIncludeUpperCase(e.target.checked)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Add Lowercase Letters</label>
+            <input
+              checked={includeLowerCase}
+              type="checkbox"
+              id="lowercase-letters"
+              name="lowercase-letters"
+              onChange={(e) => setIncludeLowerCase(e.target.checked)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input
+              checked={includeNumbers}
+              type="checkbox"
+              id="include-numbers"
+              name="include-numbers"
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input
+              checked={includeSymbols}
+              type="checkbox"
+              id="include-symbols"
+              name="include-symbols"
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
+            />
+          </div>
+          <button onClick={handleGeneratePassword} className="generator__btn">
+            Generate Password
+          </button>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </div>
+      </div>  
+    </div>
   )
 }
 
